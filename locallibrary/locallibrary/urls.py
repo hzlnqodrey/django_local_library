cacff5a4f018b03834343414bf742a4f79816057
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2024-07-30 06:12:07 +0700

    Add urls pattern new list

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -34,3 +34,10 @@ urlpatterns += [
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
 
+# use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
